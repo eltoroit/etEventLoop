2431514d2686e115dddc00ada039280ee91db34e
Andres Perez <aperez@salesforce.com>
2020-11-25 07:47:25 -0500

    Build my own tabset because LWC does not work.

--- src/modules/c/home/home.js
+++ src/modules/c/home/home.js
@@ -1,3 +1,35 @@
 import { LightningElement } from 'lwc';
 
-export default class Home extends LightningElement {}
+export default class Home extends LightningElement {
+    connectedCallback() {
+        Promise.resolve().then(() => {
+            let tabs = this.template.querySelectorAll('li[data-tab]');
+            tabs = Array.from(tabs);
+            let tab = tabs[0];
+            tab.classList.add('slds-is-active');
+        });
+    }
+
+    switchTab(event) {
+        let tabs;
+        let clickedId = event.target.getAttribute('data-tab');
+
+        tabs = Array.from(this.template.querySelectorAll('li[data-tab]'));
+        tabs.forEach((tab) => {
+            if (tab.attributes['data-tab'].value === clickedId) {
+                tab.classList.add('slds-is-active');
+            } else {
+                tab.classList.remove('slds-is-active');
+            }
+        });
+
+        tabs = Array.from(this.template.querySelectorAll('div[data-tab]'));
+        tabs.forEach((tab) => {
+            if (tab.attributes['data-tab'].value === clickedId) {
+                tab.classList.remove('slds-hide');
+            } else {
+                tab.classList.add('slds-hide');
+            }
+        });
+    }
+}
