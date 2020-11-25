814a91c535f618562749d6dfa9f1a38abc468668
Andres Perez <aperez@salesforce.com>
2020-11-24 23:12:50 -0500

    Updated main code to manage MY custom LWC

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { createElement } from 'lwc';
-import MyApp from 'my/app';
+import Home from 'c/home';
 import '@lwc/synthetic-shadow';
+import { createElement } from 'lwc';
 
-const app = createElement('my-app', { is: MyApp });
+const app = createElement('c-home', { is: Home });
 // eslint-disable-next-line @lwc/lwc/no-document-query
 document.querySelector('#main').appendChild(app);
