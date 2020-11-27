36fc90c6aa2a37d8a1e50b1eecd423300da2bc2d
Andres Perez <aperez@salesforce.com>
2020-11-26 19:43:00 -0500

    Images renamed

--- src/modules/c/demoBlink/demoBlink.js
+++ src/modules/c/demoBlink/demoBlink.js
@@ -1,3 +1,23 @@
 import { LightningElement } from 'lwc';
 
-export default class DemoBlink extends LightningElement {}
+export default class DemoBlink extends LightningElement {
+    isVisible = true;
+
+    onShowImage() {
+        this._updateUI(true);
+    }
+
+    onHideImage() {
+        this._updateUI(false);
+    }
+
+    onFlip() {
+        this._updateUI(!this.isVisible);
+    }
+
+    _updateUI(isVisible) {
+        this.isVisible = isVisible;
+        let image = this.template.querySelector('img[data-id="image"]');
+        image.style.visibility = isVisible ? 'visible' : 'hidden';
+    }
+}
