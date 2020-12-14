3ce88531bc4a955bdcd1efbdd452dcf3ecf08dfc
Andres Perez <aperez@salesforce.com>
2020-12-13 20:21:55 -0500

    Additional files

--- simulator/samples/DemoBlinkV2.js
+++ simulator/samples/DemoBlinkV2.js
@@ -1,1 +1,18 @@
-DemoBlinkV1.js
+function onVersion2() {
+	let counter = 0;
+	let maxTimes = 10;
+	const loop = (LCounter) => {
+		console.log(`Flip: ${LCounter}`);
+		setTimeout((TCounter) => {
+			console.log(`TCounter: ${TCounter}`);
+			if (TCounter < maxTimes) {
+				loop(TCounter);
+			} else {
+				console.log("Done");
+			}
+		}, 500, LCounter + 1);
+	};
+	loop(counter + 1);
+}
+onVersion2();
+console.log("Flipping...");
