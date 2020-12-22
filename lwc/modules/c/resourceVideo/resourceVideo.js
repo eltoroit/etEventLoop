f7c22e3870e573d38953ec5da339206a04019f21
Andres Perez <aperez@salesforce.com>
2020-12-22 14:44:03 -0500

    Recorded session video on youtube

--- lwc/modules/c/resourceVideo/resourceVideo.js
+++ lwc/modules/c/resourceVideo/resourceVideo.js
@@ -3,8 +3,19 @@ import { LightningElement, api } from 'lwc';
 export default class ResourceVideo extends LightningElement {
 	@api videoId;
 	@api videoLabel;
+	@api scale = 1;
+
+	videoSize = { w: 560, h: 315 };
+
+	get width() {
+		return Math.round(this.videoSize.w * this.scale);
+	}
+
+	get height() {
+		return Math.round(this.videoSize.h * this.scale);
+	}
 
 	get url() {
-		return `https://www.youtube.com/embed/${this.videoId}`;
+		return `https://www.youtube-nocookie.com/embed/${this.videoId}`;
 	}
 }
